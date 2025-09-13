c0706f1679abe67f1c503449808870ebc0787b57
nwvbug <nwvbug@gmail.com>
2025-09-12 21:35:31 -0700

    Final changes- prepping for launch

--- extension/sockets.js
+++ extension/sockets.js
@@ -25,8 +25,8 @@ function playSilentAudio() {
 playSilentAudio();
 
 
-const WS_URL = "http://127.0.0.1:7071"
-//const WS_URL = "https://ws.nwvbug.com"
+//const WS_URL = "http://127.0.0.1:7071"
+const WS_URL = "https://ws.nwvbug.com"
 var last_packet;
 var client_count = 0;
 var allow_remote = null
